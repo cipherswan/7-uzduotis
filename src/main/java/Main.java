100a79331f7fe92ebdf65bf6d03ee7952d0c1b27
cipherswan <leskauskas25@gmail.com>
2017-11-17 19:44:39 +0200

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,20 +27,41 @@ public class Main {
         String email = scanner.nextLine();
         person.addEmail(email);
 
-        */
+
 
         System.out.println("Phone number: ");
         System.out.print("+370 ");
         String phNumber = scanner.nextLine();
         System.out.println("+370 " + person.addPhoneNumber(phNumber));
 
-     //   System.out.print("Age: ");
-     //   int age = scanner.nextInt();
-     //   person.addAge(age);
+        System.out.print("Age: ");
+        int age = scanner.nextInt();
+        person.addAge(age);
 
-       // System.out.println("Kiek skaiciu: ");
-      //  int kiekis = scanner.nextInt();
-     //   System.out.println("Atsakymas: " + calc.add(kiekis));
+        */
 
+        System.out.println("Skaiciavimai");
+        System.out.println("----------");
+        System.out.println("1.Sudetis");
+        System.out.println("2.Atimtis");
+        System.out.println("3.");
+        System.out.println("4.");
+        int expression = scanner.nextInt();
+
+        switch(expression) {
+            case 1 :
+                System.out.print("Per kableli parasykite, kokius skaicius norite sudeti: ");
+                String sumInput = scanner.next();
+                calc.add(sumInput);
+                break;
+
+            case 2 :
+
+                break;
+
+
+            default :
+
+        }
     }
 }
--- src/test/java/CalculationsTest.java
+++ src/test/java/CalculationsTest.java
@@ -1,17 +1,34 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.xml.ws.RequestWrapper;
+
 public class CalculationsTest {
 
     Calculations calc = new Calculations();
 
     //add
 
+    @Test (expected = RuntimeException.class)
+    public final void kaiRasomiSkaiciaiYraNullIsmetamasException(){
+        calc.add("");
+    }
+
     @Test
     public final void kaiSudedamiSkaiciaiGaunamasAtskymas(){
-        calc.add(2);
-
+        Assert.assertEquals(1+2+3, calc.add("1,2,3"));
     }
+
+    @Test
+    public final void kaiSudedamiSkaiciaiTikimasiAtskyrimasKableliais(){
+        Assert.assertEquals(7+7+7, calc.add("7,7,7"));
+    }
+
+    @Test
+    public final void kaiSudedamasSkaiciusVienasJisIrIsvedamas(){
+        Assert.assertEquals(1, calc.add("1"));
+    }
+
 
 
 }
